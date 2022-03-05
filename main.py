c1fdc09598832b311cbc6263796dc0d84ecbc0a6
magprone <magpronem@gmail.com>
2022-03-05 12:30:16 +0300

    Database setup

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask, render_template
 
+from data import db_session
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 print(f' * SECRET_KEY: {os.environ.get("SECRET_KEY")}')
@@ -13,5 +15,6 @@ def main_page():
 
 
 if __name__ == '__main__':
+    db_session.global_init("db/data.db")
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
