7537219a0b8cbc4bc416d7cd559febdd565bb8eb
magprone <magpronem@gmail.com>
2022-03-05 23:26:29 +0300

    Login pages

--- data/models/users.py
+++ data/models/users.py
@@ -1,4 +1,5 @@
 import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 import sqlalchemy
 from sqlalchemy import orm
@@ -20,3 +21,9 @@ class User(SqlAlchemyBase, UserMixin, SerializerMixin):
     password = sqlalchemy.Column(sqlalchemy.String)
 
     orders = orm.relation('Order', back_populates='user')
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
