80402d255441347aabb4a29652608767b331ca4b
magprone <magpronem@gmail.com>
2022-03-05 16:23:18 +0300

    User setup requirements.txt Api setup

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import os
+import datetime
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_login import LoginManager, login_required, logout_user
 
 from data import db_session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 print(f' * SECRET_KEY: {os.environ.get("SECRET_KEY")}')
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
+    days=365
+)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
 
 
 @app.route('/')
