0e1463a00b737efd63342f8d8675c9489fd8e0a3
magprone <magpronem@gmail.com>
2022-03-05 11:59:21 +0300

    Base template and main page

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def main_page():
+    return render_template('main_page.html', title='order anywhere')
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
